3336d3b84f4603c5a82120b4c08ef8b25264a0bb
nupaT <trash_jack@mail.ru>
2021-09-22 22:10:19 +0300

    add: script for scripts.js

---
+++ script/script.js
@@ -0,0 +1,39 @@
+const buttUp = document.querySelector(".up-button");
+const buttDown = document.querySelector(".down-button");
+
+const sidebar = document.querySelector(".sidebar");
+const mainSlide = document.querySelector(".main-slide");
+const slidesCount = mainSlide.querySelectorAll("div").length;
+
+// const container = document.querySelector(".container");
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+buttUp.addEventListener("click", () => {
+  changeSlide("up");
+});
+buttDown.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+function changeSlide(direction) {
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1;
+    }
+  }
+
+  // const height = container.clientHeight;
+
+  // mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * 100}vh)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * 100}vh)`;
+}
